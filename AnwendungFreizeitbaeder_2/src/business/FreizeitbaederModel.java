8b9c4bb4c30ec5c26035ab9eb9d91abdb89df4ed
AytekinArmutcu <91993829+AytekinArmutcu@users.noreply.github.com>
2022-11-28 12:48:58 +0100

    Add files via upload

--- AnwendungFreizeitbaeder_2/src/business/FreizeitbaederModel.java
+++ AnwendungFreizeitbaeder_2/src/business/FreizeitbaederModel.java
@@ -3,19 +3,29 @@ package business;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Vector;
 
 import Factory.Creator;
 import Factory.CsvCreator;
 import Factory.Product;
 import Factory.TxtCreator;
+import ownUtil.Observable;
+import ownUtil.Observer;
 
 
-public class FreizeitbaederModel {
-	   // speichert temporaer ein Objekt vom Typ Freizeitbad
+public class FreizeitbaederModel implements Observable{
+	
     private Freizeitbad freizeitbad;
+	private static FreizeitbaederModel instance;
+	public Vector <Observer> observers = new Vector<Observer>();
 	
+	public static FreizeitbaederModel getInstance() {
+		if(instance==null)
+			instance=new FreizeitbaederModel();
+		return instance;
+	}
 	
-	public FreizeitbaederModel() {
+	private FreizeitbaederModel() {
 		
 	}
 	
@@ -44,6 +54,26 @@ public class FreizeitbaederModel {
 
 	public void setFreizeitbad(Freizeitbad freizeitbad) {
 		this.freizeitbad = freizeitbad;
+		notifyObservers();
+	}
+
+	@Override
+	public void addObserver(Observer obs) {
+		// TODO Auto-generated method stub
+		this.observers.addElement(obs);
+	}
+
+	@Override
+	public void removeObserver(Observer obs) {
+		// TODO Auto-generated method stub
+		this.observers.removeElement(obs);
+	}
+
+	@Override
+	public void notifyObservers() {
+		// TODO Auto-generated method stub
+		for(int i=0;i<observers.size();i++)
+			this.observers.elementAt(i).update();
 	}
 
 	
